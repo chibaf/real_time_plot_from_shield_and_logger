67656e91d1f7eab86cab306e1d0e91ed7013d5da
chibaf <chibaf@mac.com>
2022-01-16 23:31:24 +0900

    Add files via upload

--- real_time_plot_from_shield.py
+++ real_time_plot_from_shield.py
@@ -5,19 +5,16 @@ import matplotlib.pyplot as plt
 from read_shield_class import Shield
 
 data0=[0]*8
-data=[data0]*100
-print(data)
+data=[data0]*10
 shield=Shield()
 while True:
   try:
     array=shield.read_shield()
     if len(array)==8:
       data.pop(-1)
-      print(len(data))
       data.insert(0,array)
       rez = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
-      print(rez)
-      x=range(0, 100, 1)
+      x=range(0, 10, 1)
       plt.clf()
       plt.plot(x,rez[0])
       plt.plot(x,rez[1])
@@ -29,7 +26,6 @@ while True:
       plt.plot(x,rez[7])
       plt.pause(0.1)
   except KeyboardInterrupt:
-    print(str(data))
     print ('exiting')
     break
 exit()
